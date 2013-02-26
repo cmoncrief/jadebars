217f9f7ac8785fdbcf33b3c629dbbae85aecd117
Charles Moncrief <cmoncrief@gmail.com>
2013-02-25 21:13:58 -0600

    Updated handlebars dep
    - Updated tests to account for slight format changes in 1.0.9

--- test/fixtures/control/join.js
+++ test/fixtures/control/join.js
@@ -1,21 +1,27 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['subtest'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers; data = data || {};
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";});
+  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";
+  });
 templates['test'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers; data = data || {};
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";});
+  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";
+  });
 templates['test2'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers; data = data || {};
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
   
 
 
-  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";});
+  return "<div id=\"test\"><h1>This is a test jade file</h1></div>";
+  });
 })();
